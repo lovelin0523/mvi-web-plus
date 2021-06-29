dc88c3590633218a4aba56c8da33f7458445e12d
lovelin0523 <18356201582@163.com>
2021-06-29 09:15:46 +0800

    更

--- packages/index.js
+++ packages/index.js
@@ -20,11 +20,6 @@ const install = app=> {
 
 let all = Object.assign({install},components)
 
-//非import时将install暴露给window
-if (typeof window !== 'undefined' && window.Vue) {
-	window.mvi = all;
-}
-
 import packages from "../package.json";
 
 console.log('%c感谢使用'+packages.name+'，当前版本：%c v'+packages.version+'\n%cMVI框架完全由个人开发和维护，如果你觉得还不错的话，欢迎前往github给个star，感谢！\ngithub地址：%c'+packages.github,'color:#808080;','color:#008a00','color:#808080;','color:#008a00');
