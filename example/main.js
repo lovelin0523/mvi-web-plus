ac4f0bfe34321d31f092bb9a496e78a96ecab5c0
lovelin0523 <18356201582@163.com>
2021-08-03 10:48:21 +0800

    更

--- example/main.js
+++ example/main.js
@@ -2,8 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import mvi from "../packages"
 const app = createApp(App)
+app.use(mvi)
+app.mount('#app')
+
 app.config.globalProperties.dialogComponentWatch = function(name,type,el){
 	console.log(name,type)
 }
-app.use(mvi)
-app.mount('#app')
