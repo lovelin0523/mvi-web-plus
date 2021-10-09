9eebaf7b55aeb51e98da3bccc1fded0949a930f6
lingkai <18356201582@163.com>
2021-10-09 16:19:18 +0800

    更新

--- example/App.vue
+++ example/App.vue
@@ -1,22 +1,9 @@
 <template>
 	<m-button square @click="change">Button</m-button>
-	<div>value：{{active}}</div>
+	<div>value：{{color}}</div>
 	<div id="root" style="position: relative;width: 7rem;height: 10rem;border: 1px solid #000000;overflow: hidden;">
 		<!-- <div style="height: 10rem;"></div> -->
-		<m-tabs v-model="active">
-		    <m-tab title="标签一" disabled>
-		        <p>愿你三冬暖，愿你春不寒；愿你天黑有灯，下雨有伞。愿你路上有良人相伴。</p>
-		    </m-tab>
-		    <m-tab title="标签二">
-		        <p>待我君临天下，许你四海为家。待我了无牵挂，许你浪迹天涯。待我半生戎马。</p>
-		    </m-tab>
-		    <m-tab title="标签三">
-		        <p>我跨过山，涉过水，见过万物复苏，周而复始，如今山是你，水也是你。</p>
-		    </m-tab>
-		    <m-tab title="标签四">
-		        <p>纵然，从此一别两宽，各自欢喜，你也曾写进了他的生命里，虽非经久不衰，却最浓墨重彩。</p>
-		    </m-tab>
-		</m-tabs>
+		<m-color-picker hex v-model="color"></m-color-picker>
 
 		<!-- <div style="height: 10rem;"></div> -->
 	</div>
@@ -36,6 +23,7 @@
 				value: 20,
 				active: 0,
 				page: 1,
+				color:'#f30',
 				date: new Date(),
 				data: [{
 						name: '张三',
